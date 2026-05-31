9d50476563c92876ce1151393b91329853e59e28
Amrutha <156908852+Ammuzzamurutha@users.noreply.github.com>
2026-05-31 23:27:56 +0530

    Create basics.py

---
+++ day-01/basics.py
@@ -0,0 +1,21 @@
+#first program
+print("Hello World")
+#variables
+a=10
+b=20
+print(a+b)
+#f string
+name="John"
+print(f"My name is {name}")
+food="Pizza"
+print(f"I like {food}")
+#intgers
+x=5
+y=10
+print(f"The sum of {x} and {y} is {x+y}")
+#float
+pi=3.14
+print(f"The value of pi is approximately {pi}")
+#boolean
+is_online=True
+print(f"Is the user online? {is_online}")
